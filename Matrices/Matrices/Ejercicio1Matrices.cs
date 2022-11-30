6d209aacc1c2b34e36cdcafb742e89360451d5cf
lsanzl <lsanzl02@educastillalamancha.es>
2022-11-30 14:40:00 +0100

    ejerciciosMatrices1.0

---
+++ Matrices/Matrices/Ejercicio1Matrices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    public class Ejercicio1Matrices
+    {
+        private static Random rnd = new Random();
+        public static void Main(string[] args)
+        {
+            int[,] matriz = crearMatriz();
+            contarNumeros(matriz);
+        }
+        public static int[,] crearMatriz()
+        {
+            Console.Write("Introduzca dimensión matriz: ");
+            int dimension = Convert.ToInt32(Console.ReadLine());
+            int[,] matriz_nueva = new int[dimension, dimension];
+
+            for (int i = 0; i < matriz_nueva.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz_nueva.GetLength(1); j++)
+                {
+                    matriz_nueva[i, j] = rnd.Next(0, 10);
+                }
+            }
+            return matriz_nueva;
+        }
+        public static void contarNumeros(int[,] matriz)
+        {
+            int[] numeros = new int[10];
+            for (int i = 0; i < matriz.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < matriz.GetLength(1); j++)
+                {
+                    numeros[matriz[i, j]]++;
+                    Console.Write(" " + matriz[i, j] + " |");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("\n----- CONTEO DE NÚMEROS -----");
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine("El número {0} ha aparecido {1} veces", i, numeros[i]);
+            }
+        }
+    }
+}
---
+++ Matrices/Matrices/Ejercicio2Matrices.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    public class Ejercicio2Matrices
+    {
+        private static Random rnd = new Random();
+        public static void Main(string[] args)
+        {
+            
+        }
+        public static int[,] crearMatriz()
+        {
+            Console.Write("Introduzca dimensión matriz: ");
+            int dimension = Convert.ToInt32(Console.ReadLine());
+            int[,] matriz_nueva = new int[dimension, dimension];
+
+            for (int i = 0; i < matriz_nueva.GetLength(0); i++)
+            {
+                for (int j = 0; j < matriz_nueva.GetLength(1); j++)
+                {
+                    matriz_nueva[i, j] = rnd.Next(0, 10);
+                }
+            }
+            return matriz_nueva;
+        }
+        public static void showInfoMatriz(int[,] matriz)
+        {
+            int x;
+            int y;
+
+            do
+            {
+                Console.Write("Introduzca coordenada x: ");
+                x = Convert.ToInt32(Console.ReadLine());
+            } while (!checkPosicion(matriz, x, true));
+            do
+            {
+                Console.Write("Introduzca coordenada y: ");
+                y = Convert.ToInt32(Console.ReadLine());
+            } while (!checkPosicion(matriz, y, false)); 
+
+            Console.WriteLine("Espacio por encima: ", x - 0);
+            Console.WriteLine("Espacio por debajo: ", matriz.GetLength(1) - x);
+            Console.WriteLine("Espacio por la izqd: ", y - 0);
+            Console.WriteLine("Espacio por debajo: ", matriz.GetLength(0) - y);
+        }
+        public static int getArriba
+        public static Boolean checkPosicion(int[,] matriz, int num, Boolean opcion) {
+            Boolean valido = false;
+            if (opcion)
+            {
+                if (num <= matriz.GetLength(0))
+                {
+                    valido = true;
+                }
+            }
+            else
+            {
+                if (num <= matriz.GetLength(1))
+                {
+                    valido = true;
+                }
+            }
+            return valido;
+        }
+    }
+}
