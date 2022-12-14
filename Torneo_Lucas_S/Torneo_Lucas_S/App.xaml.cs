8686143529f9c42b581cd82deaa6f6599caa815d
lsanzl <lsanzl02@educastillalamancha.es>
2022-12-14 14:36:12 +0100

    version 0.1 torneo

---
+++ Torneo_Lucas_S/Torneo_Lucas_S/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Torneo_Lucas_S
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
