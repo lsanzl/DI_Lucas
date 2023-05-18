b75ee33666bf17e1ad910c47e6b0895b11f922de
lsanzl <lsanzl02@educastillalamancha.es>
2023-05-19 01:35:08 +0200

    ejercicio1 corregido ejercicio2 start
    1 corregido view
    2 empezado, organizado y visual lista

---
+++ Ejercicio2_Recuperacion_Lucas_Sanz/Ejercicio2_Recuperacion_Lucas_Sanz/Model/Producto.cs
@@ -0,0 +1,44 @@
+﻿using Ejercicio2_Recuperacion_Lucas_Sanz.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Recuperacion_Lucas_Sanz.Model
+{
+    public class Producto
+    {
+        public string codigo { get; set; }
+        public string descripcion { get; set; }
+        public double coste { get; set; }
+        public double precio { get; set; }
+        public string observaciones { get; set; }
+
+        public Producto(string codigo, string descripcion, double coste, double precio, string observaciones)
+        {
+            this.codigo = codigo;
+            this.descripcion = descripcion;
+            this.coste = coste;
+            this.precio = precio;
+            this.observaciones = observaciones;
+
+            ManageProducto manager = new ManageProducto();
+        }
+
+        public void insertarProducto()
+        {
+            
+        }
+        public void eliminarProducto()
+        {
+
+        }
+        public void modificarProducto()
+        {
+
+        }
+
+    }
+
+}
---
+++ Ejercicio2_Recuperacion_Lucas_Sanz/Ejercicio2_Recuperacion_Lucas_Sanz/Persistence/ManageProducto.cs
@@ -0,0 +1,26 @@
+﻿using Ejercicio2_Recuperacion_Lucas_Sanz.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_Recuperacion_Lucas_Sanz.Persistence
+{
+    class ManageProducto
+    {
+        public void insertarProducto(Producto producto)
+        {
+            DBBroker.getAgent().executeSQL("alter table ejerciciosrecuperacion.productos AUTO_INCREMENT = 1;");
+            DBBroker.getAgent().executeSQL($"insert into ejerciciosrecuperacion.productos (codigo, descripcion, coste, precio, observaciones) values ('{producto.codigo}','{producto.descripcion}','{producto.coste}','{producto.precio}','{producto.observaciones}');");
+        }
+        public void eliminarProducto(Producto producto)
+        {
+
+        }
+        public void modificarProducto(Producto producto)
+        {
+
+        }
+    }
+}
